9ac054561664b073d65e31784d2b4554fefd25a4
DKimani <kimanidan585@gmail.com>
2026-06-22 19:20:51 +0300

    Update files

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.pexels.com",
+        protocol: 'https',
+        hostname: 'images.pexels.com',
       },
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
       },
     ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
