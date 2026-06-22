d454d80c760aa8fa274ab5f603c543a0f91d163d
DKimani <kimanidan585@gmail.com>
2026-06-22 19:55:02 +0300

    Update files

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -50,7 +50,7 @@ const HeroSection: React.FC = () => {
               </span>
             </div>
 
-            <h1 className="text-3xl md:text-7xl font-black text-gray-900 leading-tight">
+            <h1 className="text-4xl md:text-7xl font-black text-gray-900 md:leading-tight">
                Crafting <span className="gradient-text">experiences</span> <br/>that become
               <span className="block gradient-text">
                  lifelong memories.
